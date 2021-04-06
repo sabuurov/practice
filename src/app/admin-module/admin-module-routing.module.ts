c72243b9636949bf3b899d33761fa7edd23a20c4
sabuurov <sabuurov.a.a23@gmail.com>
2021-04-06 23:53:26 +0600

    assignment 8

--- src/app/admin-module/admin-module-routing.module.ts
+++ src/app/admin-module/admin-module-routing.module.ts
@@ -1,8 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+// @ts-ignore
+import { NgModule } from "@angular/core";
+// @ts-ignore
+import { RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { GroupComponent } from "./group/group.component";
+import { UserComponent } from "./user/user.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : DashboardComponent},
+  {path : 'user', component : UserComponent},
+  {path : 'group', component : GroupComponent}
+];
 
+// @ts-ignore
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
--- src/app/admin-module/admin-module.module.ts
+++ src/app/admin-module/admin-module.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminModuleRoutingModule } from './admin-module-routing.module';
+import { UserComponent } from './user/user.component';
+import { GroupComponent } from './group/group.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserComponent, GroupComponent, DashboardComponent],
   imports: [
     CommonModule,
     AdminModuleRoutingModule
